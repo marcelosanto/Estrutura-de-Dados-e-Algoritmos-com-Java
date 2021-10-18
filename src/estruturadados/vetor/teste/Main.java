9a3e41d5f70b91b78c8535f1768f581a2f7e2efe
Marcelo Santos <rocha.mer21@gmail.com>
2021-10-18 15:18:12 -0300

    lista com generic types

--- src/estruturadados/vetor/teste/Main.java
+++ src/estruturadados/vetor/teste/Main.java
@@ -1,10 +1,11 @@
 package estruturadados.vetor.teste;
 
-import estruturadados.vetor.Lista;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Lista<Contato> vetor = new Lista<Contato>(5);
+        ArrayList<Contato> vetor = new ArrayList<Contato>();
+
 
         Contato c1 = new Contato("Contato 1", "13215-5585", "lols1@lols.com");
         Contato c2 = new Contato("Contato 2", "23215-5585", "lols2@lols.com");
@@ -12,13 +13,13 @@ public class Main {
         Contato c4 = new Contato("Contato 4", "43215-5585", "lols4@lols.com");
         Contato c5 = new Contato("Contato 5", "53215-5585", "lols5@lols.com");
 
-        vetor.adiciona(c1);
-        vetor.adiciona(c2);
-        vetor.adiciona(c3);
-        vetor.adiciona(c4);
-        vetor.adiciona(c5);
+        vetor.add(c1);
+        vetor.add(c2);
+        vetor.add(c3);
+        vetor.add(c4);
+        vetor.add(c5);
 
-        System.out.println("Tamanho é : " + vetor.tamanho());
+        System.out.println("Tamanho é : " + vetor.size());
         System.out.println(vetor);
 
 
